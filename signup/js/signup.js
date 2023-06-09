228867a26007fff9fd8d7e9e562bc5b478ed0c4b
Lila <dalila.analistadesistema@gmail.com>
2023-06-09 15:34:31 -0300

    melhorando o código

--- signup/js/signup.js
+++ signup/js/signup.js
@@ -1,21 +1,21 @@
-const form = document.querySelector ('#form')
-const nomeInput = document.querySelector('#nomecomple') 
-const emailInput = document.querySelector('#email')
-const nascimentoInput = document.querySelector('#datadenasc')
-const senhaInput = document.querySelector('#senha')
-const confirmarSenhaInput = document.querySelector('#confirmars')
+const formInput = document.getElementById('form')
+const nomeInput = document.querySelector('.nomecomple')
+const emailInput = document.querySelector('.email')
+const nascimImput = document.querySelector('.datanasc')
+const senhaInput = document.querySelector('.senha')
+const confSenhaInput = document.querySelector('.confirmars')
+const emailRegex = /^[a-z0-9]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
 const submit = document.querySelector('#fluentWind')
 
+submit.addEventListener("click", (e) => {
 
+  e.preventDefault();
 
-//evento que dispara as validações
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  //verifica se o nome está vazio
-  if(nomeInput.value === '') {
-
-  }
-  }
+  console.log('deu certo')
   
+  //validação do nome
+  if (nomeInput.value === "") {
+    alert("Por favor digite o nome válido.");
+    return;
+  }
 })
