d63e2b53c65f03be2ea4a850bfe92e4248208f52
Lila <dalila.analistadesistema@gmail.com>
2023-06-09 18:04:49 -0300

    javascript

--- signup/js/signup.js
+++ signup/js/signup.js
@@ -4,10 +4,10 @@ const emailInput = document.querySelector('.email')
 const nascimImput = document.querySelector('.datanasc')
 const senhaInput = document.querySelector('.senha')
 const confSenhaInput = document.querySelector('.confirmars')
-const emailRegex = /^[a-z0-9]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
-const submit = document.querySelector('#fluentWind')
+const checkInput = document.querySelector('.checkbox')
+const button = document.querySelector('#fluentWind')
 
-submit.addEventListener("click", (e) => {
+button.addEventListener("click", (e) => {
 
   e.preventDefault();
 
@@ -15,7 +15,50 @@ submit.addEventListener("click", (e) => {
   
   //validação do nome
   if (nomeInput.value === "") {
-    alert("Por favor digite o nome válido.");
+    alert("Por favor, preencha o seu nome.")
+    return;
+  } 
+
+  //verificar se o e-mail está preenchido e se é válido
+  if(emailInput.value === "" || !isEmailvalid(emailInput.value)){
+    alert("Por favor, preencha o seu e-mail");
+    return
+  }
+
+  // Vericar se senha esta preenchida
+  if(!validatePassword(senhaInput.value, 8)) {
+    alert("A senha precisa de oito digitos");
     return;
   }
-})
+  
+  
+});
+
+//função que valida e-mail
+function isEmailvalid(email) {
+
+  //criar uma regex para válidar e-mail
+  const emailRegex = new RegExp (
+    /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if(emailRegex.test(email)) {
+      return true;
+    } else {
+      return false;
+    }
+
+}
+
+
+//Função que valida a senha
+
+function validatePassword (password, minDigitos) {
+  if(password.length >= minDigitos) {
+    //senha válida
+    return true;
+  } else {
+    //senha inválida
+    return false;
+  }
+}
