ea19f50ccfc9136e9464132e077abbca4edfbab9
Lila <dalila.analistadesistema@gmail.com>
2023-06-09 13:23:24 -0300

    Fazendo as validações

---
+++ signup/js/signup.js
@@ -0,0 +1,21 @@
+const form = document.querySelector ('#form')
+const nomeInput = document.querySelector('#nomecomple') 
+const emailInput = document.querySelector('#email')
+const nascimentoInput = document.querySelector('#datadenasc')
+const senhaInput = document.querySelector('#senha')
+const confirmarSenhaInput = document.querySelector('#confirmars')
+const submit = document.querySelector('#fluentWind')
+
+
+
+//evento que dispara as validações
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  //verifica se o nome está vazio
+  if(nomeInput.value === '') {
+
+  }
+  }
+  
+})
